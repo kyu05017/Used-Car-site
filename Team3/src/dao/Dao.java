e95b8a35286542c7cf3825c75fb4ebe01bd2d7e3
kyu05017 <kyu05017@naver.com>
2022-04-13 16:07:04 +0900

    김규석

---
+++ Team3/src/dao/Dao.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Dao {
+	
+	private Connection con;
+	private PreparedStatement ps;
+	private ResultSet rs;
+	
+}
