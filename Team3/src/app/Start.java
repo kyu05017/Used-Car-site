3a418ad45e1af00b338336228ab236af24bfafb6
kyu05017 <kyu05017@naver.com>
2022-04-14 15:57:39 +0900

    dd

--- Team3/src/app/Start.java
+++ Team3/src/app/Start.java
@@ -4,8 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Start extends Application{
