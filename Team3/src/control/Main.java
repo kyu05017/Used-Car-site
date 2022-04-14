493c091a1b78dfc2827966d60989a9d3a8b7c2a5
kyu05017 <kyu05017@naver.com>
2022-04-14 16:27:35 +0900

    마스터

--- Team3/src/control/Main.java
+++ Team3/src/control/Main.java
@@ -25,15 +25,15 @@ public class Main implements Initializable{
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
 	}
-//	public void loadpage( String page ) { // loadpage ( 파일경로 )
-//		
-//		try {
-//			// 페이지(fxml) 객체화 
-//			Parent parent = FXMLLoader.load( getClass().getResource(page+".fxml") ); // 무조건 예외처리
-//			boarderPane.setCenter(parent); // 컨테이너(fxml) 가운데에 페이지 넣기 
-//		}
-//		catch( Exception e ) { // 파일이 존재하지 않을 경우 예외처리
-//			System.out.println("홈 페이지 연결 실패 "+ e); 
-//		}
-//	}
+	public void loadpage( String page ) { // loadpage ( 파일경로 )
+		
+		try {
+			// 페이지(fxml) 객체화 
+			Parent parent = FXMLLoader.load( getClass().getResource(page+".fxml") ); // 무조건 예외처리
+			boarderPane.setCenter(parent); // 컨테이너(fxml) 가운데에 페이지 넣기 
+		}
+		catch( Exception e ) { // 파일이 존재하지 않을 경우 예외처리
+			System.out.println("홈 페이지 연결 실패 "+ e); 
+		}
+	}
 }
