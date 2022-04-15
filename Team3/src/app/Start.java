c899a15f5d77e26ed9e14a76ef7bf5173dc6d568
504 <504@DESKTOP-N8P75DI>
2022-04-15 17:19:44 +0900

    오류수정

---
+++ Team3/src/app/Start.java
@@ -0,0 +1,22 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Start extends Application{
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent parent = FXMLLoader.load(getClass().getResource("/view/main.fxml"));
+		Scene scene = new Scene(parent);
+		stage.setScene(scene);
+		stage.setResizable(false);
+		stage.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
