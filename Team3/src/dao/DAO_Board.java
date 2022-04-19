1759af437e2c6d54891cb57cd8fe990eb715e2d7
DESKTOP-N8P75DI\504 <aaa>
2022-04-19 18:00:06 +0900

    글작성-날짜까지

--- Team3/src/dao/DAO_Board.java
+++ Team3/src/dao/DAO_Board.java
@@ -14,15 +14,15 @@ public class DAO_Board extends Dao {
 	//±€¿€º∫
 	public boolean write(DTO_Board dto_Board) {
 		try {
-			String sql = "insert into board(b_number, b_title, b_content, b_date, m_number, b_gr, b_view)values(?,?,?,?,?,?,?)";
+			String sql = "insert into board(b_number, b_title, b_content, m_number, b_gr, b_view)values(?,?,?,?,?,?)";
 			ps = con.prepareStatement(sql);
 			ps.setInt(1, dto_Board.getB_number());
 			ps.setString(2, dto_Board.getB_title());
 			ps.setString(3, dto_Board.getB_content());
-			ps.setString(4, dto_Board.getB_date());
-			ps.setInt(5, dto_Board.getM_number());
-			ps.setInt(6, dto_Board.getB_gr());
-			ps.setInt(7, dto_Board.getB_view());
+			
+			ps.setInt(4, dto_Board.getM_number());
+			ps.setInt(5, dto_Board.getB_gr());
+			ps.setInt(6, dto_Board.getB_view());
 			ps.executeUpdate();
 			return true;
 			
