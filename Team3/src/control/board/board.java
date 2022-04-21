9aec27cef2b9716e4ef86d7dc8510ea7af99c4fd
kyu05017 <kyu05017@naver.com>
2022-04-21 09:27:08 +0900

    마스터

--- Team3/src/control/board/board.java
+++ Team3/src/control/board/board.java
@@ -1,22 +1,30 @@
 package control.board;
 
 import java.net.URL;
+
 import java.util.ResourceBundle;
 
+import control.Home;
 import control.login.Login;
+
+import dao.DAO_Board;
+import dto.DTO_Board;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
 public class board implements Initializable{
 	
 	@FXML
-    private TableView<?> table;
+    private TableView<DTO_Board> table;
 
     @FXML
     private Label board_name;
@@ -25,6 +33,8 @@ public class board implements Initializable{
     private Button bt_write;
 
     public static int board_check = 0;
+    
+    public static DTO_Board board; //자유게시판테이블에서 클릭한 글 객체를 저장하는 객체
     
     @FXML
     void act_write(MouseEvent event) {
@@ -62,6 +72,33 @@ public class board implements Initializable{
     	else if(board_check == 2){
     		board_name.setText("자유 게시판");
     		bt_write.setVisible(true);
+    		ObservableList<DTO_Board> boardlist = DAO_Board.bdao.list(board_check);
+
+    		TableColumn tc = table.getColumns().get(0);
+    		tc.setCellValueFactory(new PropertyValueFactory<>("b_number"));
+    		
+    		tc= table.getColumns().get(1);
+    		tc.setCellValueFactory(new PropertyValueFactory<>("b_title"));
+    		
+    		tc= table.getColumns().get(2);
+    		tc.setCellValueFactory(new PropertyValueFactory<>("b_date"));
+    		
+    		tc= table.getColumns().get(3);
+    		tc.setCellValueFactory(new PropertyValueFactory<>("b_view"));
+    		
+    		table.setItems(boardlist);
+    		
+    		table.setOnMouseClicked( e -> { 
+    			System.out.println("클릭되었습니다 : " +e);
+    			//1.테이블에서 클릭한 객체를 임시객체에 저장
+    			board = table.getSelectionModel().getSelectedItem();//클릭된 board 객체 호출
+    			//2. 조회수 증가
+    			//3.페이지전환
+    			System.out.println(board.getB_title());
+    			Main_board.main_board.loadpage("/view/board/board_read");
+    		
+    		} );
+    		
     	}
     	
 
---
+++ Team3/src/dao/DAO_Board.java
@@ -0,0 +1,54 @@
+package dao;
+
+import dto.DTO_Board;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class DAO_Board extends Dao {
+	
+	public static DAO_Board bdao = new DAO_Board();
+	
+	//글작성
+	public boolean write(DTO_Board dto_Board) {
+		try {
+			String sql = "insert into board(b_title, b_content, m_number)values(?,?,?,?)";
+			ps = con.prepareStatement(sql);
+			ps.setString(1, dto_Board.getB_title());
+			ps.setString(2, dto_Board.getB_content());
+			ps.setInt(3, dto_Board.getM_number());
+			ps.setInt(4, dto_Board.getB_gr());
+			ps.executeUpdate();
+			return true;
+
+		}catch(Exception e){
+			System.out.println("DAO_Board 글작성 오류 " + e);
+		}
+		return false;
+	}
+	//자유게시판 글 호출 메소드
+	public ObservableList<DTO_Board> list(int b_number){
+		ObservableList<DTO_Board> boardlist = FXCollections.observableArrayList();
+		try {
+			String sql = "select * from board where b_gr= ? order by b_number";
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, b_number);
+			rs=ps.executeQuery();
+			while(rs.next()) {
+				DTO_Board board = new DTO_Board(rs.getInt(1),
+						rs.getString(2),
+						rs.getString(3),
+						rs.getString(4),
+						rs.getInt(5),
+						rs.getInt(6),
+						rs.getInt(7)
+						);
+				boardlist.add(board);
+				
+			}		
+			return boardlist;
+		}catch(Exception e) {
+			System.out.println( "DAO_Board 글호출 오류 "+e  );
+		}
+		return null;
+	}
+}
