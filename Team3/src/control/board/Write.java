c2f929add4f14bf43dfb56f907bb4a3dd6095813
DESKTOP-N8P75DI\504 <aaa>
2022-04-21 14:39:41 +0900

    merge

--- Team3/src/control/board/Write.java
+++ Team3/src/control/board/Write.java
@@ -3,10 +3,7 @@ package control.board;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-<<<<<<< HEAD
-=======
 import control.login.Login;
->>>>>>> refs/remotes/origin/master
 import dao.DAO_Board;
 import dto.DTO_Board;
 import javafx.event.ActionEvent;
@@ -45,11 +42,7 @@ public class Write implements Initializable{
     	String b_title = txttitle.getText();
     	String b_content = txtcontents.getText();
     	
-<<<<<<< HEAD
-    	DTO_Board dto_Board = new DTO_Board(0, b_title, b_content, null, 0, 0, 0);
-=======
     	DTO_Board dto_Board = new DTO_Board(0, b_title, b_content, null, Login.member.getM_number(), board.board_check,0);
->>>>>>> refs/remotes/origin/master
     	
     	boolean result = DAO_Board.bdao.write(dto_Board);
     	if(result) {
@@ -68,24 +61,13 @@ public class Write implements Initializable{
     
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    	if(Admin_board.board_check == 2){
-    		txttitle.setText("자유 게시판"); 
-=======
-=======
 
->>>>>>> refs/remotes/origin/master
     	if(board.board_check == 1) {
     		lbl_title.setText("공지 사항");
     	}
     	else if(board.board_check == 2){
     		lbl_title.setText("자유 게시판");
-<<<<<<< HEAD
->>>>>>> refs/remotes/origin/master
-=======
 
->>>>>>> refs/remotes/origin/master
     	}
     }
 }
--- Team3/src/dao/DAO_Board.java
+++ Team3/src/dao/DAO_Board.java
@@ -1,75 +1,5 @@
 package dao;
 
-<<<<<<< HEAD
-
-
-
-import dto.DTO_Board;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-
-public class DAO_Board extends Dao {
-	
-	
-
-	public static DAO_Board bdao = new DAO_Board();
-	
-	//글작성
-	public boolean write(DTO_Board dto_Board) {
-		try {
-			String sql = "insert into board(b_number, b_title, b_content, m_number)values(?,?,?,?)";
-			ps = con.prepareStatement(sql);
-			ps.setInt(1, dto_Board.getB_number());
-			ps.setString(2, dto_Board.getB_title());
-			ps.setString(3, dto_Board.getB_content());
-//			, m_number, b_gr, b_view
-			ps.setInt(4, dto_Board.getM_number());
-//			ps.setInt(5, dto_Board.getB_gr());
-//			ps.setInt(6, dto_Board.getB_view());
-			ps.executeUpdate();
-			return true;
-			
-			
-			
-		}catch(Exception e){
-			System.out.println("DAO_Board 글작성 오류 " + e);
-		}
-		return false;
-	}
-	//자유게시판 글 호출 메소드
-	public ObservableList<DTO_Board> list(){
-		ObservableList<DTO_Board> boardlist = FXCollections.observableArrayList();
-		try {
-			String sql = "select * from board order by b_number asc";
-			
-			ps = con.prepareStatement(sql);
-			
-			rs=ps.executeQuery();
-			
-			while(rs.next()) {
-				
-				DTO_Board board = new DTO_Board(rs.getInt(1),
-						rs.getString(2),
-						rs.getString(3),
-						rs.getString(4),
-						rs.getInt(5),
-						rs.getInt(6),
-						rs.getInt(7)
-						);
-				boardlist.add(board);
-				
-			}
-			
-			return boardlist;
-		}catch(Exception e) {
-			System.out.println( "DAO_Board 글호출 오류 "+e  );
-			
-		}
-		return null;
-	}
-	
-}
-=======
 import dto.DTO_Board;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -122,4 +52,3 @@ public class DAO_Board extends Dao {
 		return null;
 	}
 }
->>>>>>> refs/remotes/origin/master
