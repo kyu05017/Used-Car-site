32b196089fc8f141c471319184c0302feae5f5d4
DESKTOP-N8P75DI\504 <aaa>
2022-04-21 18:11:01 +0900

    dd

--- Team3/src/control/board/Read.java
+++ Team3/src/control/board/Read.java
@@ -1,13 +1,17 @@
 package control.board;
 
 import java.net.URL;
+
 import java.util.Optional;
 import java.util.ResourceBundle;
 
 import control.login.Login;
 import dao.DAO_Board;
 import dao.DAO_Member;
+import dao.DAO_Reply;
 import dto.DTO_Board;
+import dto.DTO_Reply;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -16,8 +20,10 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 
 public class Read implements Initializable{
@@ -56,7 +62,7 @@ public class Read implements Initializable{
     private TextArea txt_recontents;
 
     @FXML
-    private TableView<?> re_talbe;
+    private TableView<DTO_Reply> re_talbe;
 
     @FXML
     private Button bt_redelete;
@@ -64,6 +70,8 @@ public class Read implements Initializable{
     @FXML
     private Button btreupdate;
 
+    public static DTO_Reply reply;
+    
     @FXML
     void back(ActionEvent event) {
     	if(board.board_check == 1) {
@@ -95,12 +103,67 @@ public class Read implements Initializable{
 
     @FXML
     void re_update(ActionEvent event) {
-
+    	
+    	String reply_contents = txt_recontents.getText();
+    	
+    	if(txt_recontents.getText().equals("")) {
+    		Alert alert = new Alert(AlertType.INFORMATION);
+    		alert.setHeaderText("내용을 입력해주세요.");
+    		alert.showAndWait();
+    	}
+    	else {
+    		if(Login.member == null) {
+    			Alert alert = new Alert(AlertType.INFORMATION);
+        		alert.setHeaderText("로그인후 이용가능 합니다.");
+        		alert.showAndWait();
+        		txt_recontents.setText("");	
+        		return;
+    		}
+    		else {
+    			
+//	        	boolean result = DAO_Reply.rdao.re_update();
+//	        	
+//	        	if(result) {
+//	        		Alert alert = new Alert(AlertType.INFORMATION);
+//	        		alert.setHeaderText("댓글 작성이 완료 되었습니다.");
+//	        		alert.showAndWait();
+//	        		txt_recontents.setText("");	
+//	        	}
+    		}
+    	}
     }
 
     @FXML
     void re_write(ActionEvent event) {
-
+    	
+    	String reply_contents = txt_recontents.getText();
+    	
+    	if(txt_recontents.getText().equals("")) {
+    		Alert alert = new Alert(AlertType.INFORMATION);
+    		alert.setHeaderText("내용을 입력해주세요.");
+    		alert.showAndWait();
+    	}
+    	else {
+    		if(Login.member == null) {
+    			Alert alert = new Alert(AlertType.INFORMATION);
+        		alert.setHeaderText("로그인후 이용가능 합니다.");
+        		alert.showAndWait();
+        		txt_recontents.setText("");	
+        		return;
+    		}
+    		else {
+    			DTO_Reply reply = new DTO_Reply(0,Login.member.getM_number(),board.board.getB_number(),reply_contents,null);
+        	
+	        	boolean result =DAO_Reply.rdao.re_write(reply);
+	        	
+	        	if(result) {
+	        		Alert alert = new Alert(AlertType.INFORMATION);
+	        		alert.setHeaderText("댓글 작성이 완료 되었습니다.");
+	        		alert.showAndWait();
+	        		txt_recontents.setText("");	
+	        	}
+    		}
+    	}
     }
 
     boolean upcheck = true; //수정 스위치 변수
@@ -155,6 +218,28 @@ public class Read implements Initializable{
 		blb_date.setText("작성일 : "+board.board.getB_date());
 		lbl_view.setText("조회수 : "+board.board.getB_view());
 		txt_contents.setText(board.board.getB_content());
+		bt_redelete.setVisible(false);
+		btreupdate.setVisible(false);
+		txt_contents.setEditable(false);
+		re_talbe.setOnMouseClicked( e -> {
+			try {
+				reply = re_talbe.getSelectionModel().getSelectedItem();
+				if(reply.getM_number() == Login.member.getM_number()) {
+					bt_redelete.setVisible(true);
+					btreupdate.setVisible(true);
+					bt_rewrite1.setVisible(false);
+				}
+				else {
+					btreupdate.setVisible(false);
+					bt_redelete.setVisible(false);
+					bt_rewrite1.setVisible(true);
+				}
+			}
+			catch(Exception e2) {
+				System.out.println("[존재하지 않는 댓글] 사유 " + e);
+			}
+		});
+		
 		
 //		if(writer.equals(DAO_Member.mdao.get_member(writer))) {
 //			bt_delete.setVisible(true);//버튼보이기
@@ -171,5 +256,32 @@ public class Read implements Initializable{
 			bt_update.setVisible(false);
 		}
 		
+	}
+	public void reply_show() {
+		
+		ObservableList<DTO_Reply> replyList =  DAO_Reply.rdao.list(board.board.getB_number());
+		
+		String writer = null;
+		
+		for(DTO_Reply reply : replyList) {
+			
+			writer = DAO_Member.mdao.get_id(reply.getM_number());
+			
+		}
+		
+		
+		TableColumn<?, ?> tc = re_talbe.getColumns().get(0);
+		tc.setCellValueFactory(new PropertyValueFactory<>("r_number"));
+		
+		tc= re_talbe.getColumns().get(1);
+		tc.setCellValueFactory(new PropertyValueFactory<>("writer"));
+		
+		tc= re_talbe.getColumns().get(2);
+		tc.setCellValueFactory(new PropertyValueFactory<>("r_content"));
+		
+		tc= re_talbe.getColumns().get(3);
+		tc.setCellValueFactory(new PropertyValueFactory<>("r_date"));
+
+		re_talbe.setItems(replyList);
 	}
 }
---
+++ Team3/src/dao/DAO_Reply.java
@@ -0,0 +1,70 @@
+package dao;
+
+import dto.DTO_Reply;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class DAO_Reply extends Dao {
+	
+	public DAO_Reply() {}
+	
+	public static DAO_Reply rdao = new DAO_Reply();
+	
+	public boolean re_write(DTO_Reply reply) {
+		try {
+			String sql = "INSERT INTO reply(m_number,b_number,r_content) VALUES (?,?,?)";
+			ps = con.prepareStatement(sql);
+			ps.setInt(1,reply.getM_number());
+			ps.setInt(2,reply.getB_number());
+			ps.setString(3,reply.getR_content());
+			ps.executeUpdate();
+			return true;
+		}
+		catch (Exception e) {
+			System.out.println("댓글 저장 실패 " + e);
+		}
+		return false;
+	}
+	//댓글 호오출
+	public ObservableList<DTO_Reply> list(int b_number){
+		ObservableList<DTO_Reply> replyList = FXCollections.observableArrayList();
+		try {
+			String sql = "select * from reply where b_number= ? order by r_number";
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, b_number);
+			rs=ps.executeQuery();
+			while(rs.next()) {
+				DTO_Reply reply = new DTO_Reply(
+						rs.getInt(1),
+						rs.getInt(2),
+						rs.getInt(3),
+						rs.getString(4),
+						rs.getString(5)
+						);
+				replyList.add(reply);
+			}		
+			return replyList;
+		}catch(Exception e) {
+			System.out.println( "DAO_Board 글호출 오류 "+e  );
+		}
+		return null;
+	}
+	public boolean re_update(int r_number,String r_content) {
+		try {
+			
+		}
+		catch(Exception e) {
+			System.out.println("댓글 수정 오류 " + e);
+		}
+		return false;
+	}
+	public boolean re_delete() {
+		try {
+			
+		}
+		catch(Exception e) {
+			System.out.println("댓글 삭제 오류 " + e);
+		}
+		return false;
+	}
+}
