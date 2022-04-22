6af1a1077423f4da00a2fabce9e1d2e99cd028e3
kyu05017 <kyu05017@naver.com>
2022-04-22 15:45:48 +0900

    마스터

--- Team3/src/control/car/Carlist.java
+++ Team3/src/control/car/Carlist.java
@@ -1,6 +1,7 @@
 package control.car;
 
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 import dao.DAO_Car;
@@ -47,39 +48,36 @@ public class Carlist implements Initializable{
 			System.out.println("중고차 불러오기");
 			System.out.println(car.getC_title());
 		}
-		// 2. 그리드 클래스 [ 행/열 ]
 		GridPane gridPane = new GridPane();
-			// * 그리드 여백
 			gridPane.setPadding( new Insets(10) );
-			// * 그리드내 셀 여백 
-			gridPane.setHgap(200); // 세로여백 
-			gridPane.setVgap(300); // 가로여백
 
-		int i = 0 ; // 인덱스용 변수
+			gridPane.setHgap(200); 
+			gridPane.setVgap(300); 
+
+		int i = 0 ;
 		for( int row = 0 ; row < carlist.size(); row++ ) { // 행
-				// 1. 이미지
+
 				ImageView imageView = new ImageView( new Image( carlist.get(i).getC_img()) );
-					// *이미지 사이즈 
-					imageView.setFitWidth(200);		// 이미지 가로길이 
-					imageView.setFitHeight(100); 	// 이미지 세로길이
-				// 2. 버튼 생성 ( 버튼에 이미지 넣기 )
-				Button button = new Button( null , imageView );
-					// *버튼 배경제거 [ transparent : 투명색 ]
+
+					imageView.setFitWidth(200);
+					imageView.setFitHeight(100); 
+					DecimalFormat df = new DecimalFormat("#,##0원");
+					String new_price = df.format(carlist.get(i).getC_price());
+				Button button = new Button("차량명 : "+ carlist.get(i).getC_title() + "                                                                      가격 : "+ new_price + " | 주행거리 : " + carlist.get(i).getC_km() + "km |  연식 : "+ carlist.get(i).getC_year(), imageView );
+					
 					button.setStyle("-fx-background-color:transparent");
-					// *버튼 id값넣기 [ 제품 식별 = index ]
+
 					button.setId( i+"");
-					// *버튼 클릭이벤트
-						//	button.setOnAction( e -> { } ); // 람다식 : 인수 -> { 실행코드 }
+					
 					button.setOnAction( e -> { 
-						// 1. 클릭한 버튼의 id 가져오기
+
 						int id = Integer.parseInt(e.toString().split(",")[0].split("=")[2] );
-						// 2. 클릭한 제품 번호 저장 
+				
 						select = carlist.get(id);
 					} );	
-				gridPane.add( button  , 0, row); // 그리드내  해당 열번호 , 행번호 에 버튼 추가
-				i++; //인덱스 증가
+				gridPane.add( button  , 0, row); 
+				i++;
 		}
-		// 4. vbox에 그리도 넣기 
 		vbox.getChildren().add(gridPane);
     		
     }
