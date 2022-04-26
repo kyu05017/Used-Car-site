5da4d018ea3b718003db855a32041531f8a9d07b
kyu05017 <kyu05017@naver.com>
2022-04-26 16:20:30 +0900

    마스터

--- Team3/src/control/letter/letterview.java
+++ Team3/src/control/letter/letterview.java
@@ -4,16 +4,21 @@ package control.letter;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import control.car.Carlist;
 import control.login.Login;
 import dao.DAO_Letter;
+import dao.DAO_Member;
 import dto.DTO_Letter;
 import javafx.event.ActionEvent;
 	import javafx.fxml.FXML;
 	import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 	import javafx.scene.control.TextArea;
 	import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 	public class letterview implements Initializable {
 
@@ -36,16 +41,44 @@ import javafx.scene.text.Text;
 
 	    @FXML
 	    void write(ActionEvent event) {
-	    	String l_content = txtcontent.getText();
-	    	String m_id = Login.member.getM_id();
+	    	String msg = txtcontent.getText();
+	    	Alert alert2 = new Alert(AlertType.INFORMATION);
 	    	
+	    	if(msg.equals("")) {
+	    		alert2.setTitle("알림창");
+	    		alert2.setHeaderText(" 내용을 입력해주세요. ");
+	    		alert2.setContentText("확인");
+	    		alert2.showAndWait();
+	    		txtcontent.requestFocus();
+	    	}
+	    	else {
+	    		String dealer_id = letter1.letter.getM_id();
+	    		
+	    		DTO_Letter letter = new DTO_Letter(0,Login.member.getM_number(),dealer_id, msg,null,letter1.letter.getC_number());
+	    			
+	    		boolean result = DAO_Letter.dao_Letter.send(letter);
+	    		
+	    		if(result) {
+	    			txtcontent.setText("");
+	    			get();
+	    		}
+	    	}
 	    	
 	    }
 
 		@Override
 		public void initialize(URL arg0, ResourceBundle arg1) {
 			
+			get();
+			
+
+		}
+		
+		void get() {
+			txtcontentlist.setText("");
+			
 			ArrayList<DTO_Letter> letters = DAO_Letter.dao_Letter.get(letter1.letter.getC_number(),letter1.letter.getM_number(),Login.member.getM_id());
+			
 			
 			for(DTO_Letter temp : letters) {
 				if(temp.getM_number() == Login.member.getM_number()) {
@@ -56,8 +89,6 @@ import javafx.scene.text.Text;
 				}
 				System.out.println(temp.getL_content());
 			}
-			
-
 		}
 
 }
